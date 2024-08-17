d9b3857a235dac882028c4b66b8f551da9664c94
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-08-17 20:28:24 +0530

    Add Global Usings

---
+++ src/Matchmaker.Infra.Http/Usings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Net.Http.Headers;
+global using static Microsoft.AspNetCore.Http.StatusCodes;
+global using IEndpoints = Microsoft.AspNetCore.Routing.IEndpointRouteBuilder;
