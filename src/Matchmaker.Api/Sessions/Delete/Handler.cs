b146de0b9a54a470bcb68bb4678eaaef0ab9524e
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-08-18 20:56:54 +0530

    Update DeleteSession feature to also delete from queue

--- src/Matchmaker.Api/Sessions/Delete/Handler.cs
+++ src/Matchmaker.Api/Sessions/Delete/Handler.cs
@@ -1,15 +1,21 @@
 namespace Matchmaker.Sessions.Delete;
 
+using System.Threading.Channels;
+
 public interface IHandler
 {
   Task<IResult> Handle(Command command);
 }
 
-public sealed class Handler(IStore store) : IHandler
+public sealed class Handler(
+  IStore store,
+  ChannelWriter<IMatchRequest> queue) : IHandler
 {
   public async Task<IResult> Handle(Command command)
   {
     bool deleted = await store.DeleteSession(command.UserId);
+
+    await queue.WriteAsync(new DeleteRequest(command.UserId));
 
     return deleted ? new DeleteResult() : new NotFoundResult();
   }
