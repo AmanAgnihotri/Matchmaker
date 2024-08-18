9043b0b01744a019d59afbc4132c045ebcc39d3b
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-08-18 20:51:48 +0530

    Add IMatchmakingStore

---
+++ src/Matchmaker.Domain/Sessions/Common/Stores/MatchmakingStore.cs
@@ -0,0 +1,20 @@
+namespace Matchmaker.Sessions;
+
+public interface IMatchmakingStore
+{
+  void AddOrUpdate(User user);
+
+  bool TryRemoveUser(UserId userId, out User? user);
+
+  IEnumerable<User> GetWaitingUsers();
+
+  void Add(Session session);
+
+  Session? GetSession(SessionId sessionId);
+
+  IEnumerable<Session> GetSessions();
+
+  void RemoveFullSessions(int maxUsersPerSession);
+
+  Task Save(UserId userId, SessionId sessionId, TimeSpan maxRetainTime);
+}
