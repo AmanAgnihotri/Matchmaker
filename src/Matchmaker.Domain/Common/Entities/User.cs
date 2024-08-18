47199b7e307e5eae469bde3555df4a162b696360
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-08-18 20:52:43 +0530

    Add CancellationTokenSource to User

--- src/Matchmaker.Domain/Common/Entities/User.cs
+++ src/Matchmaker.Domain/Common/Entities/User.cs
@@ -6,4 +6,31 @@ public sealed record User(
   DateTime QueueTime)
 {
   public SessionId? SessionId { get; set; }
+
+  private CancellationTokenSource? _source;
+
+  public CancellationToken GetOrCreateCancellationToken()
+  {
+    if (_source is not null)
+    {
+      return _source.Token;
+    }
+
+    _source = new CancellationTokenSource();
+
+    return _source.Token;
+  }
+
+  public void TryCancel()
+  {
+    if (_source is null)
+    {
+      return;
+    }
+
+    _source.Cancel();
+    _source.Dispose();
+
+    _source = null;
+  }
 }
