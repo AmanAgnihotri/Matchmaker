633a53b54576fb165099a79c7f48ad26a1cd42be
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-08-23 04:04:28 +0530

    Add MatchmakingService with AddUser and MatchUsers

---
+++ src/Matchmaker.Domain/Sessions/Common/Services/MatchmakingService.cs
@@ -0,0 +1,30 @@
+namespace Matchmaker.Sessions;
+
+public sealed class MatchmakingService(
+  MatchmakingConfig config,
+  MatchmakingState state)
+{
+  public bool AddUser(User user)
+  {
+    return state.AddUser(user);
+  }
+
+  public List<User>? MatchUsers(DateTime time)
+  {
+    List<User> matchedUsers = [];
+
+    foreach (User user in state.GetWaitingUsers()
+               .OrderBy(user => user.QueueTime)
+               .TakeWhile(_ => matchedUsers.Count < config.MaxUsersPerSession))
+    {
+      if (config.MatchCriteria.Any(c => c.Matches(user, matchedUsers, time)))
+      {
+        matchedUsers.Add(user);
+      }
+    }
+
+    return matchedUsers.Count >= config.MinUsersPerSession
+      ? matchedUsers
+      : null;
+  }
+}
